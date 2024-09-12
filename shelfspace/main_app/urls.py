39591c49838fdcc21158b8a3949fec35805fce9d
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-09-12 13:48:05 -0500

    Added navbar and routes for creating a book

--- shelfspace/main_app/urls.py
+++ shelfspace/main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('books/', views.books_index, name='books_index'),  # List all books
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),  # Book detail view
+    path('books/new/', views.book_create, name='book_create'),  # Add Book Form
 ]
--- shelfspace/main_app/views.py
+++ shelfspace/main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
+from .forms import BookForm
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +12,14 @@ def books_index(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'books/detail.html', {'book': book})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books_index')
+    else:
+        form = BookForm()
+
+    return render(request, 'books/book_form.html', {'form': form})
