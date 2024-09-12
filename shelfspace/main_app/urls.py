305fda21202b13cd86ea89b2f9b4b1d00717998a
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-09-12 13:17:18 -0500

    Added routes for book index, details and admins

--- shelfspace/main_app/urls.py
+++ shelfspace/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('books/', views.books_index, name='books_index'),  # List all books
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),  # Book detail view
 ]
