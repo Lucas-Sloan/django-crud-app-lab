3d9b0abaf7ecdbe334dbe04c71fd601fcc27e7c0
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-09-12 14:26:05 -0500

    Adjusted navbar to base.html/ adjusted templates

--- shelfspace/main_app/urls.py
+++ shelfspace/main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('books/', views.books_index, name='books_index'),  # List all books
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),  # Book detail view
     path('books/new/', views.book_create, name='book_create'),  # Add Book Form
+    path('books/<int:book_id>/edit/', views.book_update, name='book_update'),  # Update Book
+    path('books/<int:book_id>/delete/', views.book_delete, name='book_delete'),  # Delete Book
 ]
--- shelfspace/main_app/views.py
+++ shelfspace/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 from .forms import BookForm
 
@@ -23,3 +23,21 @@ def book_create(request):
         form = BookForm()
 
     return render(request, 'books/book_form.html', {'form': form})
+
+def book_update(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', book_id=book.id)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/book_form.html', {'form': form, 'book': book})
+
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books_index')
+    return render(request, 'books/book_confirm_delete.html', {'book': book})
